bf96b0b7a687d8545f2fcb2882d2b1b937cda756
mainangaruiya <003paulmaina@gmail.com>
2024-07-18 11:28:08 +0300

    loops 0-3 for loop

--- classwork/loops/if1.java
+++ classwork/loops/if1.java
@@ -1,3 +1,8 @@
+//0-3 ascending order
 public class if1{
-	
+	public static void main(String[] args) {
+		for (int counter = 0; counter<=3; counter++) {
+			System.out.println(counter);
+		}		
+	}
 }
