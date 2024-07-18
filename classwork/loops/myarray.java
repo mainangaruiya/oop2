1021d416513c4d453e8c44e4569cfa8a051b655f
mainangaruiya <003paulmaina@gmail.com>
2024-07-18 12:06:15 +0300

    loops array

---
+++ classwork/loops/myarray.java
@@ -0,0 +1,11 @@
+//arrays 
+public class myarray{
+	public static void main(String[] args) {
+
+int [] javascores  = new int[7];
+//acces element in index 1
+System.out.println(javascores[1]);
+
+		//for (int counter = 10; counter >=0; counter--) {}		
+	}
+}
