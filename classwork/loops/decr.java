95370ded8645b5f298d0c8b6b2a87d6de2ba8d49
mainangaruiya <003paulmaina@gmail.com>
2024-07-18 11:41:16 +0300

    loops 0-3 for decrement

---
+++ classwork/loops/decr.java
@@ -0,0 +1,8 @@
+//10-0  desscending order
+public class decr{
+	public static void main(String[] args) {
+		for (int counter = 10; counter >=0; counter--) {
+			System.out.println(counter);
+		}		
+	}
+}
