62d84f5d199d228446f729414b25f04a3558c7d0
mainangaruiya <003paulmaina@gmail.com>
2024-07-25 13:12:49 +0300

    methods

--- classwork/methods/Rectangle.java
+++ classwork/methods/Rectangle.java
@@ -1,11 +1,23 @@
+package methodspkg;
+import java.util.Scanner;
+
 public class Rectangle {
     public static void main(String[] args) {
-        int length = 10, width = 5, area, perimeter;
+        Scanner input = new Scanner(System.in);
+        int length, width, area, perimeter;
+
+        System.out.println("Enter the length:");
+        length = input.nextInt();
+
+        System.out.println("Enter the width:");
+        width = input.nextInt();
+
         area = length * width;
         perimeter = 2 * (length + width);
 
         System.out.println("Area is: " + area);
         System.out.println("Perimeter is: " + perimeter);
 
+        input.close();
     }
 }
