5cee088ba96cb39ef735d2272f60382c343ec87d
mainangaruiya <003paulmaina@gmail.com>
2024-07-25 12:01:42 +0300

    methods

---
+++ classwork/methods/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        int length = 10, width = 5, area, perimeter;
+        area = length * width;
+        perimeter = 2 * (length + width);
+
+        System.out.println("Area is: " + area);
+        System.out.println("Perimeter is: " + perimeter);
+    }
+}
