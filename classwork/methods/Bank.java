792c5a6803fea64c8e0fbf4f24f34a201a20a47f
mainangaruiya <003paulmaina@gmail.com>
2024-08-02 11:09:19 +0300

    new changes

---
+++ classwork/methods/Bank.java
@@ -0,0 +1,11 @@
+/**java program to store and manage atm transaction data
+should have 4 maina functionalities 
+1 check balance 
+2 withdraw_money 
+3 transfer to another account within same bank wher transfer chargres is 0 or to another bank where charges are 0.001% of amount tranfered
+4 deposit/recieved money 
+nd dont send/withdrawal kore than you have 
+**/
+class Bank {
+
+}
--- classwork/methods/Rect.java
+++ classwork/methods/Rect.java
@@ -1,32 +1,31 @@
 import java.util.Scanner;
 
 public class Rect {
-    Scanner scan = new Scanner(System.in);
 
-   
-    public void getDimensions() {
+    public static void getDimensions() {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter the length of rectangle: ");
         double length = scan.nextDouble();
 
         System.out.println("Enter the width of rectangle: ");
-       double width = scan.nextDouble();
-       computeAreaPerimeter(length, width);
+        double width = scan.nextDouble();
+        
+        computeAreaPerimeter(length, width);
+        scan.close();
     }
 
-    public void computeAreaPerimeter(double length, double width) {
-       double area = length * width;
-       double perimeter = 2 * (length + width);
-       displayValues(area, perimeter);
-
+    public static void computeAreaPerimeter(double length, double width) {
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+        displayValues(area, perimeter);
     }
 
-    public void displayValues(double area, double perimeter) {
+    public static void displayValues(double area, double perimeter) {
         System.out.println("The area of the rectangle is: " + area);
         System.out.println("The perimeter of the rectangle is: " + perimeter);
     }
 
     public static void main(String[] args) {
-        Rect rectobj1 = new Rect();
-        rectobj1.getDimensions();
+        getDimensions();
     }
 }
